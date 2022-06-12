ee0cf4b44db387060dbe26434a4947e548b27e03
sixiaobing167 <107335881+sixiaobing167@users.noreply.github.com>
2022-06-12 18:20:15 +0900

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ip_addr" {
-  value = aws_instance.amazon_linux_2.private_ip
+  value = aws_instance.amazon_linux_2999.private_ip
 }
 
 output "instance_public_ip_addr" {
-  value = aws_instance.amazon_linux_2.public_ip
+  value = aws_instance.amazon_linux_2999.public_ip
 }
 
 output "instance_public_sg" {
-  value = aws_instance.amazon_linux_2.security_groups
+  value = aws_instance.amazon_linux_2999.security_groups
 }
