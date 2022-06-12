896f1539d11a657d12b19058a3701abb72623635
sixiaobing167 <107335881+sixiaobing167@users.noreply.github.com>
2022-06-12 18:12:06 +0900

    Add files via upload
    aa

--- main.tf
+++ main.tf
@@ -105,25 +105,9 @@ resource "aws_security_group" "qyt_aws_allow_ssh_web" {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["670568029210"] # Canonical
-}
-
 resource "aws_instance" "amazon_linux_2" {
   key_name      = "new-aws"
-  ami           =  data.aws_ami.ubuntu.id
+  ami           = "ami-0cbec04a61be382d9"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.qyt_outside_subnet.id
   iam_instance_profile = "WebService"
